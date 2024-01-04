fa2d262c609194f83fec92c562bf5142c2269557
Nitasneem Liala <6610110160@email.psu.ac.th>
2024-01-04 20:39:20 +0700

    main file

---
+++ main.py
@@ -0,0 +1,25 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.graphics import BorderImage
+
+
+class Board(Widget):
+    def __init__(self, **kwargs):
+        super(Board, self).__init__(**kwargs)
+
+    def board_background(self):
+        with self.canvas.before:
+            BorderImage(pos=self.pos, 
+                        size=self.size, 
+                        source='board.png')
+
+
+class MyGameApp(App):
+    def on_start(self):
+        board = self.root.ids.board
+        board.board_background()
+
+
+if __name__ == '__main__':
+    MyGameApp().run()
+
